93995fd2a1f15e8c331d5fe283c51bd97b8c8770
AzraelSec <federicogerardi94@gmail.com>
2021-06-27 19:36:47 +0200

    frontend: Regen connectors

---
+++ frontend/src/connectors/models/index.ts
@@ -0,0 +1,6 @@
+export * from './ModelsCreateProposalRequest';
+export * from './ModelsCreateSessionRequest';
+export * from './ModelsCreateVoteRequest';
+export * from './ModelsProposal';
+export * from './ModelsSession';
+export * from './ModelsVote';

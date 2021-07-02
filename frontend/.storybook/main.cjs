6eab430f663f0bac21fdddd7539ef2331ef06b8d
Antonio Pitasi <antonio@pitasi.dev>
2021-07-02 22:15:18 +0200

    init storybook for doing component driven development

---
+++ frontend/.storybook/main.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf"
+  ],
+  "svelteOptions": {
+    // https://github.com/storybookjs/storybook/issues/11587
+    // "preprocess": require("../svelte.config.js").preprocess
+  }
+}

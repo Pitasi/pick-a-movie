28dcae97bbcb8332638503f06203e847683dd5d6
AzraelSec <federicogerardi94@gmail.com>
2021-07-13 23:44:38 +0200

    backend: Add `Session.Title` model

--- backend/models/session.go
+++ backend/models/session.go
@@ -4,6 +4,7 @@ import "time"
 
 type Session struct {
 	Id        int64       `json:"id" pg:"id,pk" validate:"required"`
+	Title     string      `json:"title" example:"a beautiful title"`
 	StartAt   time.Time   `json:"startAt" validate:"required" example:"2011-10-05T14:48:00.000Z"`
 	EndAt     time.Time   `json:"endAt" validate:"required" example:"2011-10-05T14:48:00.000Z"`
 	Proposals []*Proposal `json:"proposals" pg:"rel:has-many"`
@@ -11,6 +12,7 @@ type Session struct {
 } //@name Session
 
 type CreateSessionRequest struct {
+	Title   string    `json:"title"`
 	StartAt time.Time `json:"startAt" validate:"required"`
 	EndAt   time.Time `json:"endAt" validate:"required"`
 } //@name CreateSessionRequest
